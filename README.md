c6a63688da995b1d44b448601249b6a50e2ef70b
Oussama Douhou <oussama.douhou@gmail.com>
2024-07-31 21:34:19 +0200

    module testing

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # vps-plesk
-# vps-plesk
---
+++ install-plesk.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+echo "Prepairing to update kernel"
+apt update
+
+echo "Prepairing to upgrade kernel"
+apt upgrade -y
+
+echo "Excecuting Plesk installation"
+sh <(curl https://autoinstall.plesk.com/one-click-installer || wget -O - https://autoinstall.plesk.com/one-click-installer)
+
+
+echo "enable Apache modules"
+
+plesk sbin httpd_modules_ctl -e cgi mpm_prefork passenger sysenv
